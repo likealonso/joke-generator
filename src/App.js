13b4120284f9b805f5e313a33751b29c7533443c
Alonso Tacanga <alonso.tacanga@gmail.com>
2019-12-26 17:08:02 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -2,25 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      joke: null
+    }
+  }
+  onTellJoke = () => {
+    fetch('https://icanhazdadjoke.com/', {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json'
+      }
+    }).then(response => response.json())
+      .then(json => {
+        this.setState({
+          joke: json.joke
+
+        })
+        console.log(json.joke)
+      }
+      )
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.onTellJoke}>Tell me a joke!</button>
+        <p>{this.state.joke}</p>
+      </div>
+    )
+  }
+
+
 }
 
 export default App;
